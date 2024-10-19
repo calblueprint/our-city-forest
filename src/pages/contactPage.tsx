8769cb6613a1317d42d93ec3dbf0bb09b6d5ed14
katyhonguyen <katynguyen@berkeley.edu>
2024-10-18 18:59:36 -0700

    lo-fi contact page

--- src/pages/contactPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { styles } from '../../styles';
-
-export default function App() {
-  <View style={styles.container}>
-    <Text>Contact</Text>
-  </View>;
-}

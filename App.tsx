fca7930b4ba3188550d2cdd1d8666cfdaf5154dc
katyhonguyen <katynguyen@berkeley.edu>
2024-10-30 18:05:27 -0700

    updated contact page

--- App.tsx
+++ App.tsx
@@ -1,42 +1,23 @@
 import React from 'react';
-import { Image, Text, View } from 'react-native';
-import { styles } from './styles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ContactPage from './src/screens/contactPage';
+import SearchScreen from './src/screens/searchScreen';
+
+export type RootStackParamList = {
+  Search: undefined;
+  Contact: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={{
-            uri: 'https://images.squarespace-cdn.com/content/v1/545bbd7ee4b032c1794c4020/1502738972151-U1234630JTWT5NEITDFG/image-asset.jpeg?format=1500w',
-          }}
-          style={styles.responsiveImage}
-        />
-
-        <View style={styles.overlay}>
-          <Text style={styles.Heading4}>Contact Us</Text>
-        </View>
-      </View>
-
-      <View style={styles.contactInfo}>
-        <View>
-          <Text style={styles.iconColor}> IconPlaceholder</Text>
-        </View>
-        <Text style={styles.contactText}>
-          123 Berkeley Way, San Jose, CA 95035
-        </Text>
-
-        <View>
-          <Text style={styles.iconColor}> IconPlaceholder</Text>
-        </View>
-        <Text style={styles.managerText}>Nursery Manager</Text>
-        <Text style={styles.contactText}>123 - 456 - 7890</Text>
-
-        <View>
-          <Text style={styles.iconColor}> IconPlaceholder</Text>
-        </View>
-        <Text style={styles.contactText}>nurserymanager@ourcityforest.org</Text>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Contact" component={ContactPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }

56eb98012d7f7e9939d3a3a1295b7048fb5286e4
katyhonguyen <katynguyen@berkeley.edu>
2024-11-18 15:14:17 -0800

    fixed: commit issues

--- App.tsx
+++ App.tsx
@@ -1,54 +1,71 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import QRCodeScanner from '@/components/QRCodeScanner/QRCodeScanner';
-import AdminLoginScreen from '@/screens/AdminLoginScreen';
+import AllTreesScreen from '@/screens/all_trees/AllTreesScreen';
+import AvailableTreesScreen from '@/screens/available_trees/AvailableTreesScreen';
+import ContactPage from '@/screens/ContactPage/contactPage';
 import HomeScreen from '@/screens/Home/Home';
-import LoginScreen from '@/screens/LoginScreen';
+import LoginScreen from '@/screens/login/LoginScreen';
+import SearchScreen from '@/screens/SearchScreen/SearchScreen';
 import TreeInfoPage from '@/screens/TreeInfo/TreeInfo';
 import { LoginStackParamList, RootStackParamList } from '@/types/navigation';
 
-const LoginStack = createNativeStackNavigator<LoginStackParamList>();
 const RootStack = createNativeStackNavigator<RootStackParamList>();
+const LoginStack = createNativeStackNavigator<LoginStackParamList>();
 
-function LoginStackNavigator() {
-  return (
-    <LoginStack.Navigator initialRouteName="Login">
-      <LoginStack.Screen name="Login" component={LoginScreen} />
-      <LoginStack.Screen name="AdminLogin" component={AdminLoginScreen} />
-    </LoginStack.Navigator>
-  );
-}
+DefaultTheme.colors.background = '#FFFFFF';
 
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-function RootStackNavigator() {
+const App = () => {
   return (
-    <RootStack.Navigator initialRouteName="Home">
-      <RootStack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="Scanner"
-        component={QRCodeScanner}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="TreeInfoPage"
-        component={TreeInfoPage}
-        options={{ headerShown: false }}
-      />
-    </RootStack.Navigator>
-  );
-}
+    <NavigationContainer theme={DefaultTheme}>
+      <LoginStack.Navigator initialRouteName="Login">
+        <LoginStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <LoginStack.Screen
+          name="AllTrees"
+          component={AllTreesScreen}
+          options={{ headerShown: false }}
+        />
+        <LoginStack.Screen
+          name="AvailableTrees"
+          component={AvailableTreesScreen}
+          options={{ headerShown: false }}
+        />
+      </LoginStack.Navigator>
 
-function App() {
-  return (
-    <NavigationContainer>
-      <LoginStackNavigator />
+      <RootStack.Navigator initialRouteName="Home">
+        <RootStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="Scanner"
+          component={QRCodeScanner}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="TreeInfoPage"
+          component={TreeInfoPage}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ headerShown: false }}
+        />
+        <RootStack.Screen
+          name="Contact"
+          component={ContactPage}
+          options={{ headerShown: false }}
+        />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;

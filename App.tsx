e58432427b7de5f11f657bd5c928e3dc9e49523b
Carolyn Zhuang <cwz@berkeley.edu>
2024-11-20 16:32:06 -0800

    fixed

--- App.tsx
+++ App.tsx
@@ -1,57 +1,34 @@
-import * as React from 'react';
+import React from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import QRCodeScanner from '@/components/QRCodeScanner/QRCodeScanner';
 import AllTreesScreen from '@/screens/all_trees/AllTreesScreen';
 import AvailableTreesScreen from '@/screens/available_trees/AvailableTreesScreen';
-import HomeScreen from '@/screens/Home/Home';
 import LoginScreen from '@/screens/login/LoginScreen';
-import TreeInfoPage from '@/screens/TreeInfo/TreeInfo';
-import { LoginStackParamList, RootStackParamList } from '@/types/navigation';
+import { LoginStackParamList } from '@/types/navigation';
 
-const RootStack = createNativeStackNavigator<RootStackParamList>();
-const LoginStack = createNativeStackNavigator<LoginStackParamList>();
-DefaultTheme.colors.background = '#FFFFFF';
-
+const Stack = createNativeStackNavigator<LoginStackParamList>();
 DefaultTheme.colors.background = '#FFFFFF';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <LoginStack.Navigator initialRouteName="Login">
-        <LoginStack.Screen
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ headerShown: false }}
         />
-        <LoginStack.Screen
-          name="AllTrees"
-          component={AllTreesScreen}
-          options={{ headerShown: false }}
-        />
-        <LoginStack.Screen
+        <Stack.Screen
           name="AvailableTrees"
           component={AvailableTreesScreen}
           options={{ headerShown: false }}
         />
-      </LoginStack.Navigator>
-      {/* <RootStack.Navigator initialRouteName="Home">
-        <RootStack.Screen
-          name="Home"
-          component={HomeScreen}
+        <Stack.Screen
+          name="AllTrees"
+          component={AllTreesScreen}
           options={{ headerShown: false }}
         />
-        <RootStack.Screen
-          name="Scanner"
-          component={QRCodeScanner}
-          options={{ headerShown: false }}
-        />
-        <RootStack.Screen
-          name="TreeInfoPage"
-          component={TreeInfoPage}
-          options={{ headerShown: false }}
-        />
-      </RootStack.Navigator> */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }

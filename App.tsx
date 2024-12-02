e64788b8a839bcc1d73d0ba6abbbbf9a747c5dca
Katy Nguyen <123796321+katyhonguyen@users.noreply.github.com>
2024-12-01 23:13:09 -0800

    feat: lo-fi contact page (#5)
    * added contact page
    
    * updated contact page
    
    * fixed duplicates and prettier checks
    
    ---------
    
    Co-authored-by: Chris Torres <christophertorres@berkeley.edu>

--- App.tsx
+++ App.tsx
@@ -1,54 +1,23 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import QRCodeScanner from '@/components/QRCodeScanner/QRCodeScanner';
-import AdminLoginScreen from '@/screens/AdminLoginScreen';
-import HomeScreen from '@/screens/Home/Home';
-import LoginScreen from '@/screens/LoginScreen';
-import TreeInfoPage from '@/screens/TreeInfo/TreeInfo';
-import { LoginStackParamList, RootStackParamList } from '@/types/navigation';
+import ContactPage from './src/screens/contactPage';
+import SearchScreen from './src/screens/searchScreen';
 
-const LoginStack = createNativeStackNavigator<LoginStackParamList>();
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+export type RootStackParamList = {
+  Search: undefined;
+  Contact: undefined;
+};
 
-function LoginStackNavigator() {
-  return (
-    <LoginStack.Navigator initialRouteName="Login">
-      <LoginStack.Screen name="Login" component={LoginScreen} />
-      <LoginStack.Screen name="AdminLogin" component={AdminLoginScreen} />
-    </LoginStack.Navigator>
-  );
-}
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-function RootStackNavigator() {
-  return (
-    <RootStack.Navigator initialRouteName="Home">
-      <RootStack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="Scanner"
-        component={QRCodeScanner}
-        options={{ headerShown: false }}
-      />
-      <RootStack.Screen
-        name="TreeInfoPage"
-        component={TreeInfoPage}
-        options={{ headerShown: false }}
-      />
-    </RootStack.Navigator>
-  );
-}
-
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <LoginStackNavigator />
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Contact" component={ContactPage} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
---
+++ src/screens/searchScreen.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import {
+  FlatList,
+  ImageBackground,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../App';
+import { fetchTreeData } from '../supabase/client';
+import { styles } from './styles/styles';
+
+type SearchScreenProps = NativeStackScreenProps<RootStackParamList, 'Search'>;
+
+export default function SearchScreen({ navigation }: SearchScreenProps) {
+  const [trees, setTrees] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadTreeData = async () => {
+      const treeData = await fetchTreeData();
+      console.log('Fetched trees:', treeData);
+      if (treeData) {
+        setTrees(treeData);
+      }
+    };
+
+    loadTreeData();
+  }, []);
+
+  const renderTreeCard = ({ item }: { item: any }) => (
+    <View style={styles.treeRow}>
+      <View style={styles.treeCard}>
+        <ImageBackground
+          source={{
+            uri: item.image_url || 'https://example.com/placeholder-image.jpg',
+          }}
+          style={styles.treeImage}
+        ></ImageBackground>
+        <View>
+          <Text style={styles.treeName}>{item.species}</Text>
+          <View style={styles.treeDetails}>
+            <Text style={styles.treeInfo}>Row {item.row}</Text>
+            <Text style={styles.treeInfo}> • </Text>
+            <Text style={styles.treeInfo}>Bank {item.bank}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+
+  return (
+    <ScrollView style={styles.backgroundContainer}>
+      <TouchableOpacity onPress={() => navigation.navigate('Contact')}>
+        <Text>Contact Us</Text>
+      </TouchableOpacity>
+
+      <View style={styles.searchContainer}>
+        <Text style={styles.Heading4Search}>Trees Availibility</Text>
+
+        <FlatList
+          data={trees}
+          renderItem={renderTreeCard}
+          keyExtractor={item => item.tree_id.toString()}
+        />
+      </View>
+    </ScrollView>
+  );
+}

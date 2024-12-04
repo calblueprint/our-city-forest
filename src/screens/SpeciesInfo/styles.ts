c6747f139678ea953ce68823768c64cc1cfa86d4
Chris Torres <christophertorres@berkeley.edu>
2024-12-04 15:44:44 -0800

    add empty screens for Directory and SpeciesInfo

---
+++ src/screens/SpeciesInfo/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+    color: '#333',
+  },
+});
+
+export default styles;

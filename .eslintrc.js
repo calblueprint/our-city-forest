9d73ed5162caa1c31b80daa3f8c6639f1c4e0fd1
Alex Wang <73456512+AlexWang05@users.noreply.github.com>
2024-12-11 04:17:31 -0800

    QR Generation & Storage: Supabase Edge Function [OCF-18] (#10)
    * Basic supabase queries to add and remove trees
    
    * Complete testing of query functions, change uuid handling of multiple trees
    
    * Add tree info queries & setters for all properties
    
    * Wrote QR generation edge function
    
    ---------
    
    Co-authored-by: Aditya Pawar <34043950+adityapawar1@users.noreply.github.com>
    Co-authored-by: Aditya Pawar <apawar@berkeley.edu>
    Co-authored-by: Chris Torres <99312117+christophertorres1@users.noreply.github.com>
    Co-authored-by: Chris Torres <christophertorres@berkeley.edu>

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-shadow': 'error',
   },
+  ignorePatterns: ['src/supabase/functions/'],
   settings: {
     'import/resolver': {
       typescript: {

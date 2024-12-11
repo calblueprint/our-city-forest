fbff3e75f770692faf4e02876e97394045bfb6b1
carolynzhuang <142048122+carolynzhuang@users.noreply.github.com>
2024-12-11 04:13:06 -0800

    Cwz/ocf 33 auth context (#19)
    * Added auth context for login
    
    ---------
    
    Co-authored-by: Chris Torres <christophertorres@berkeley.edu>

--- src/components/GoogleSignInButton/GoogleSignInButton.tsx
+++ src/components/GoogleSignInButton/GoogleSignInButton.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import { makeRedirectUri } from 'expo-auth-session';
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useAuth } from '@/context/AuthContext';
 import { LoginStackParamList, RootStackParamList } from '@/types/navigation';
 import { styles } from './styles';
 
@@ -16,76 +16,50 @@ type GoogleSignInButtonProps = CompositeScreenProps<
   NativeStackScreenProps<RootStackParamList, 'BottomTabs'>
 >;
 
-type UserInfo = {
-  email: string;
-  name: string;
-};
-
 export default function GoogleSignInButton({
   navigation,
 }: GoogleSignInButtonProps) {
-  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-  const [request, response, promptAsync] = Google.useAuthRequest({
+  const { setAuthenticated } = useAuth();
+  const [, response, promptAsync] = Google.useAuthRequest({
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
     iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
     redirectUri: makeRedirectUri({ scheme: 'org.calblueprint.ourcityforest' }),
   });
 
-  console.log(request);
-  console.log(userInfo);
-
   useEffect(() => {
-    async function handleSignInWithGoogle() {
+    const handleSignInWithGoogle = async (token: string) => {
       try {
-        const userJSON = await AsyncStorage.getItem('@user');
-        if (userJSON) {
-          setUserInfo(JSON.parse(userJSON));
-        } else if (
-          response?.type === 'success' &&
-          response.authentication?.accessToken
-        ) {
-          await getUserInfo(response.authentication.accessToken);
+        const userResponse = await fetch(
+          'https://www.googleapis.com/userinfo/v2/me',
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          },
+        );
+
+        if (userResponse.ok) {
+          await setAuthenticated(true);
           navigation.navigate('BottomTabs', {
             screen: 'Home',
             params: { screen: 'TreeSearch' },
           });
+        } else {
+          console.error('Authentication failed');
+          await setAuthenticated(false);
         }
       } catch (error) {
-        console.error('Error retrieving user data from AsyncStorage:', error);
+        console.error('Failed to sign in with Google:', error);
+        await setAuthenticated(false);
       }
-    }
+    };
 
-    handleSignInWithGoogle();
-  }, [navigation, response]);
-
-  const getUserInfo = async (token: string) => {
-    if (!token) return;
-    try {
-      const userResponse = await fetch(
-        'https://www.googleapis.com/userinfo/v2/me',
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        },
-      );
-      const user = await userResponse.json();
-      await AsyncStorage.setItem('@user', JSON.stringify(user));
-      setUserInfo(user);
-    } catch (error) {
-      console.error('Failed to fetch user data:', error);
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      handleSignInWithGoogle(response.authentication.accessToken);
     }
-  };
+  }, [response, navigation, setAuthenticated]);
 
   return (
-    <TouchableOpacity
-      onPress={() => {
-        promptAsync();
-        navigation.navigate('BottomTabs', {
-          screen: 'Home',
-          params: { screen: 'TreeSearch' },
-        });
-      }}
-    >
+    <TouchableOpacity onPress={() => promptAsync()}>
       <Text style={styles.adminLoginLinkText}>Login Here</Text>
     </TouchableOpacity>
   );

5e39a13cdb03b83b8b702fdc11c9e40988f65a65
eshabansiya <esha_bansiya@berkeley.edu>
2025-03-15 11:43:51 -0700

    fix lint

--- src/components/TreesInformationCard/styles.ts
+++ src/components/TreesInformationCard/styles.ts
@@ -1,6 +1,4 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '@/styles/colors';
-import { typography } from '@/styles/typography';
 
 export const styles = StyleSheet.create({
   centeredView: {

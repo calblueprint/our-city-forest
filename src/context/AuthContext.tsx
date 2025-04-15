37aa6fe31e4b491af77aa83d4348b6b49d6a022c
Carolyn Zhuang <cwz@berkeley.edu>
2025-04-14 21:07:06 -0700

    login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,7 +5,7 @@ import React, {
   useEffect,
   useState,
 } from 'react';
-import { Alert } from 'react-native';
+// import { Alert } from 'react-native';
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
 import AsyncStorage from '@react-native-async-storage/async-storage';
